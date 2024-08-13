851510d06f6de005b7d8113daa3216ea3613a869
Mostafa Ismail <77923860+MostafaIsmaiel@users.noreply.github.com>
2024-08-13 15:15:22 +0300

    update absolute path

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 import withPlaiceholder from '@plaiceholder/next';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 
 const withNextIntl = createNextIntlPlugin();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  transpilePackages: ['lucide-react'],
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
   images: {
     remotePatterns: [
       {
