cc5bd9d9493db40956a2dcebe1e864498cc62bca
Mostafa Ismail <77923860+MostafaIsmaiel@users.noreply.github.com>
2024-08-19 14:24:22 +0300

    Update page.tsx

--- src/app/[locale]/(Pages)/service/page.tsx
+++ src/app/[locale]/(Pages)/service/page.tsx
@@ -1,14 +1,17 @@
-'use server';
+import { notFound, type RedirectType } from 'next/navigation';
 
-import { getServices } from '@/app/lib/Controller';
-import { AppPathnames } from '@/config';
 import { redirect } from '@/navigation';
-import type { RedirectType } from 'next/navigation';
+import { AppPathnames } from '@/config';
+import { getServices } from '@/app/lib/Controller';
 
 const Services = async () => {
   const {
     services: { services },
   } = await getServices();
+
+  if (!services && !!!services.length) {
+    return null;
+  }
 
   redirect(
     `/service/${services[0].slug}` as AppPathnames,
