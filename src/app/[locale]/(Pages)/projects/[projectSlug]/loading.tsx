280a4c2172e31727415f9d4d730cb691cf5d1363
Mostafa Ismail <77923860+MostafaIsmaiel@users.noreply.github.com>
2024-12-25 11:15:21 +0200

    Add error and loading components for project pages

---
+++ src/app/[locale]/(Pages)/projects/[projectSlug]/loading.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import LoadingCircular from '@/app/Components/Molecules/Loading/LoadingCircular';
+
+const Loading = () => {
+  return <LoadingCircular />;
+};
+
+export default Loading;
